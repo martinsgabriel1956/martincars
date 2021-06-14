9f2c587bfc4294f7310ef48ddd5aa8c446fcb692
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-14 15:46:24 -0300

    feat: challenge 32 finished

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,32 +1,30 @@
 /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-  Essas informações devem ser adicionadas no HTML via Ajax.
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
+Já temos as funcionalidades de adicionar e remover um carro. Agora, vamos persistir esses dados, 
+salvando-os temporariamente na memória de um servidor.
+Nesse diretório do `challenge-32` tem uma pasta `server`. É um servidor simples, em NodeJS, para 
+que possamos utilizar para salvar as informações dos nossos carros.
+Para utilizá-lo, você vai precisar fazer o seguinte:
+- Via terminal, acesse o diretório `server`;
+- execute o comando `npm install` para instalar as dependências;
+- execute `node app.js` para iniciar o servidor.
+Ele irá ser executado na porta 3000, que pode ser acessada via browser no endereço: 
+`http://localhost:3000`
+O seu projeto não precisa estar rodando junto com o servidor. Ele pode estar em outra porta.
+As mudanças que você irá precisar fazer no seu projeto são:
+- Para listar os carros cadastrados ao carregar o seu projeto, faça um request GET no endereço
+`http://localhost:3000/car`
+- Para cadastrar um novo carro, faça um POST no endereço `http://localhost:3000/car`, enviando
+os seguintes campos:
+  - `image` com a URL da imagem do carro;
+  - `brandModel`, com a marca e modelo do carro;
+  - `year`, com o ano do carro;
+  - `plate`, com a placa do carro;
+  - `color`, com a cor do carro.
+Após enviar o POST, faça um GET no `server` e atualize a tabela para mostrar o novo carro cadastrado.
+Crie uma branch `challenge-32` no seu projeto, envie um pull request lá e cole nesse arquivo a URL
+do pull request.
+*/
+
 function app() {
   let elements = {
     nameInterprese: document.querySelector("#nameInterprese"),
@@ -37,31 +35,34 @@ function app() {
     form: document.querySelector("form"),
   };
   
+  
   elements.form.addEventListener("submit", (e) => {
     e.preventDefault();
+    features.insertInfo();
     features.registerCar();
-  });
 
+  });
+  
   const features = {
     showInfo() {
       fetch("./company.json")
-        .then((res) => res.json())
-        .then((data) => {
-          elements.nameInterprese.innerHTML = data.name;
-          elements.telInterprese.innerHTML = data.tel;
-        });
+      .then((res) => res.json())
+      .then((data) => {
+        elements.nameInterprese.innerHTML = data.name;
+        elements.telInterprese.innerHTML = data.tel;
+      });
     },
     registerCar() {
       const tr = document.createElement("tr");
-
+      
       elements.inputs.forEach((input) => {
         const td = document.createElement("td");
-
+        
         if (!input.value) {
           alert("Please enter a value");
           throw new Error("erro");
         }
-
+        
         td.innerHTML = input.value;
         input.value = "";
         tr.appendChild(td);
@@ -72,13 +73,13 @@ function app() {
     removeCar() {
       const td = document.createElement("td");
       const removeBtn = document.createElement("button");
-
+      
       removeBtn.textContent = "remover carro";
       removeBtn.classList.add("removeBtn");
       removeBtn.addEventListener("click", features.remove);
-
+      
       td.appendChild(removeBtn);
-
+      
       return td;
     },
     remove(e) {
@@ -86,7 +87,27 @@ function app() {
       const tr = this.parentNode.parentNode;
       tr.classList.add("removeClass");
     },
+    insertInfo() {
+      let image = document.querySelector("#imgCar").value;
+      let brandModel = document.querySelector("#modelCar").value;
+      let year = document.querySelector("#yearCar").value;
+      let plate = document.querySelector("#plateCar").value;
+      let color = document.querySelector("#colorCar").value;
+      
+      fetch('http://localhost:3000', {
+        method: 'POST', body: JSON.stringify({
+          image,
+          brandModel,
+          year,
+          plate,
+          color
+        })
+      }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(e => console.log(e))
+    }
   };
+  
   
   window.onload = features.showInfo();
 }
